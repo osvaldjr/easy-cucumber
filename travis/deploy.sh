6ae35b23dd3a7de6f34a5263ee42c848a28975e7
Lucas Carnevalli <verdi@hotmail.com.br>
2019-06-24 10:03:44 -0300

    deploy

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-openssl aes-256-cbc -K $encrypted_a1fc9919b14a_key -iv $encrypted_a1fc9919b14a_iv -in secrets.tar.enc -out secrets.tar -d && tar xvf secrets.tar
+openssl aes-256-cbc -K $encrypted_a1fc9919b14a_key -iv $encrypted_a1fc9919b14a_iv -in ./secrets.tar.enc -out ./secrets.tar -d && tar xvf ./secrets.tar
 mvn deploy -B --settings ./settings.xml -DperformRelease=true
