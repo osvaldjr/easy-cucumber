daaa8ad708c7dac991d1338ad85f566ca6404482
osvaldjr <osvald.jr@gmail.com>
2019-10-09 09:08:20 -0300

    Simple Spring 5 application and fix DBConfig

--- src/main/java/io/github/osvaldjr/confs/DatabaseConfig.java
+++ src/main/java/io/github/osvaldjr/confs/DatabaseConfig.java
@@ -3,12 +3,22 @@ package io.github.osvaldjr.confs;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableAutoConfiguration(
+    exclude = {
+      DataSourceAutoConfiguration.class,
+      DataSourceTransactionManagerAutoConfiguration.class,
+      HibernateJpaAutoConfiguration.class
+    })
 public class DatabaseConfig {
 
   @Value("${spring.datasource.url:}")
