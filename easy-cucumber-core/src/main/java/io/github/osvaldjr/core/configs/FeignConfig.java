6d65effe02ad2a9a165d9ca1650bc0fd5bfad9fa
osvaldjr <osvald.jr@gmail.com>
2020-02-14 00:03:41 -0300

    removing feign config from mocks - already defined in core

--- easy-cucumber-core/src/main/java/io/github/osvaldjr/core/configs/FeignConfig.java
+++ easy-cucumber-core/src/main/java/io/github/osvaldjr/core/configs/FeignConfig.java
@@ -16,7 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
-@EnableFeignClients(basePackages = {"io.github.osvaldjr.clients"})
+@EnableFeignClients(basePackages = {"io.github.osvaldjr"})
 public class FeignConfig {
 
   @Bean
