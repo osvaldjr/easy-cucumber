66eed8a8e2eb9e2bad5e170e74217c4bf7620e53
Lucas Carnevalli <verdi@hotmail.com.br>
2020-02-13 23:32:21 -0300

    Example add

--- examples/simple-springboot-core/src/test/java/example/simple/springboot/core/steps/ContextLoader.java
+++ examples/simple-springboot-core/src/test/java/example/simple/springboot/core/steps/ContextLoader.java
@@ -3,8 +3,15 @@ package example.simple.springboot.core.steps;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import cucumber.api.java.Before;
+import example.simple.springboot.core.SimpleSpringbootCoreApplication;
+import io.github.osvaldjr.core.EasyCucumberContextLoader;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
+    classes = {
+      SimpleSpringbootCoreApplication.class,
+      EasyCucumberContextLoader.class,
+    })
 public class ContextLoader {
 
   @Before
