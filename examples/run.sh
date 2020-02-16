973ade8f971a53e6f10a0faae3c49d9278143196
Lucas Carnevalli <verdi@hotmail.com.br>
2020-02-15 22:51:28 -0300

    Run examples

--- examples/run.sh
+++ examples/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 {
-    docker-compose up -d
+    docker-compose up -d &&
     mvn clean verify -f simple-springboot-core/pom.xml &&
     mvn clean verify -f simple-springboot-activemq/pom.xml &&
     mvn clean verify -f simple-springboot-datasource/pom.xml &&
     mvn clean verify -f simple-springboot-mockserver/pom.xml &&
+    rm -rf simple-springboot-owaspzap/src/test/resources/session/*session* &&
     mvn clean verify -f simple-springboot-owaspzap/pom.xml &&
     mvn clean verify -f simple-springboot-stubby/pom.xml &&
     mvn clean verify -f complete-springboot/pom.xml
