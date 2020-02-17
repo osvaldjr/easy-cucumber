7aa7e7fd796161ed54825ce1cedb5b7f6626997d
osvaldjr <osvald.jr@gmail.com>
2020-02-16 22:53:40 -0300

    change deploy config

--- examples/run.sh
+++ examples/run.sh
@@ -6,7 +6,7 @@
     mvn clean verify -f simple-springboot-datasource/pom.xml &&
     mvn clean verify -f simple-springboot-mockserver/pom.xml &&
     rm -rf simple-springboot-owaspzap/src/test/resources/session/*session* &&
-    mvn clean verify -f simple-springboot-owaspzap/pom.xml &&
+    mvn clean verify -f simple-springboot-owaspzap/pom.xml -Deasycucumber.target.url="http://$(ip -4 addr show scope global dev docker0 | grep inet | awk '{print $2}' | cut -d / -f 1):9000" &&
     mvn clean verify -f simple-springboot-stubby/pom.xml &&
     mvn clean verify -f complete-springboot/pom.xml
 } || {
